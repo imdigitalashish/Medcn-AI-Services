e8f8ee12941adce63030d54d054b24a2d85387cb
imdigitalashish <imdigitalashish@gmail.com>
2025-09-02 11:28:15 +0530

    CORS ADDED

--- core/server.py
+++ core/server.py
@@ -2,12 +2,22 @@ from fastapi import FastAPI
 from api import router
 from dotenv import load_dotenv
 
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 def create_app() -> FastAPI:
     app = FastAPI(
         title="Medcn AI Services",
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
     app.include_router(router, prefix="/api")
     return app
